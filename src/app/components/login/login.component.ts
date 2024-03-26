46bea71ccb8033c79b230bec81049c4ffafbd9e3
DavidAmaya2001 <gamerbloom5@gmail.com>
2024-03-26 09:41:46 -0600

    Login changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit{
 
-  
+  //* Parametro local donde almacena los usuarios que son consumidos del API por el service
   public getUser: User[] = [];
 
   constructor( 
@@ -20,7 +20,9 @@ export class LoginComponent implements OnInit{
     private router: Router,
     ){}
 
+  //* Codigo que se inicializa junto al componente
   ngOnInit(): void {
+    //* Subscripcion al Observable para que mande la peticion a la API y obtener todos los usuarios almacenados
     this.creedentialService.getUserCredentials().subscribe(
       users => {
         this.getUser = users;
@@ -29,13 +31,23 @@ export class LoginComponent implements OnInit{
     );
   }
 
+  //* Parametro que almacena la data ingresada por el usuario en el formulario (form)
   form = new FormGroup({
     username: new FormControl(''),
     password: new FormControl('')
   })
 
+  //* Parametro booleano de acceso de usuario
   public validationUser: boolean = false;
 
+  //* Funcion que se ejecuta cuando el evento Submit del Form (form) es ejecutado
+  onSubmit(){
+    this.getUser.forEach(user => {
+      this.validation(user);
+    })
+  }
+
+  //* Funcion que ejecuta un recorrido de todos los usuarios de la API contra las credenciales ingresadas por el usuario
   validation(user: User){
     if(this.form.value.username === user.user && this.form.value.password === user.password){
       this.validationUser = true;
@@ -47,10 +59,5 @@ export class LoginComponent implements OnInit{
   }
 
 
-  onSubmit(){
-    this.getUser.forEach(user => {
-      this.validation(user);
-    })
-  }
   
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const regexdui = /^[0-9]{8}-[0-9]$/;
+const regexnit = /^[0-9]{4}-[0-9]/;
 
 @Component({
   selector: 'shared-modal',
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
   
+  
+
 }
